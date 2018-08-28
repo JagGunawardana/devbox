acd727d276e75532173338428ddc9f2e3b2a5f8d
Jagath Gunawardana <jagath.gunawardana@NL-JAGATH-MBP.local>
2018-08-28 22:27:18 +0100

    Add generated files to speed things up

---
+++ .emacs.d/custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a24c5b3c12d147da6cef80938dca1223b7c7f70f2f382b26308eba014dc4833a" "9b1c580339183a8661a84f5864a6c363260c80136bd20ac9f00d7e1d662e936a" default))
+ '(ohai-personal-taste/paredit t)
+ '(ohai-personal-taste/run-wizard nil)
+ '(ohai-personal-taste/splash 'daily-otter)
+ '(ohai-personal-taste/style 'dark)
+ '(ohai-personal-taste/training-wheels nil)
+ '(ohai-personal-taste/window-state 'maximised)
+ '(ohai/modules
+   '(ohai-appearance ohai-fonts ohai-general ohai-splash ohai-ido ohai-navigation ohai-editing ohai-complete ohai-snippets ohai-codestyle ohai-dired ohai-project ohai-flycheck ohai-git ohai-orgmode ohai-help ohai-elisp ohai-helm ohai-eshell ohai-unicode ohai-refactor ohai-emoji ohai-smart-mode-line ohai-ext-window-nav ohai-html ohai-markdown ohai-javascript ohai-clojure))
+ '(package-selected-packages
+   '(cider clojure-mode tern js2-refactor js2-mode json-mode markdown-mode rainbow-mode web-mode smart-mode-line rich-minority emojify emr unicode-fonts eshell-git-prompt helm-flx helm-projectile highlight-parentheses discover-my-major which-key org-cliplink org-bullets org-plus-contrib git-gutter-fringe gist magit helm-flycheck flycheck-color-mode-line flycheck projectile dired+ quelpa-use-package quelpa ethan-wspace company-try-hard company-emoji company-quickhelp company volatile-highlights shrink-whitespace expand-region multiple-cursors anzu ace-window avy flx-ido ido-vertical-mode smex ido-completing-read+ web material-theme diminish nyan-mode hlinum airline-themes powerline paredit cheatsheet ox-reveal plantuml-mode graphviz-dot-mode racket-mode bm sr-speedbar go-eldoc yasnippet swiper-helm helm-go-package go-mode exec-path-from-shell f use-package paradox)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
