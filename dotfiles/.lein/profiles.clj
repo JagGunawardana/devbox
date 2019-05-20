a53b5a1e6c8261dd47ca072342804ffc39e7133b
Jag Gunawardana <jag.gunawardana@Jags-MacBook-Pro.local>
2019-05-20 10:32:48 +0100

    Add lein midje plugin

--- dotfiles/.lein/profiles.clj
+++ dotfiles/.lein/profiles.clj
@@ -6,7 +6,8 @@
                   [lein-pprint "1.1.2"]
                   [com.jakemccrary/lein-test-refresh "0.24.1"]
                   [lein-autoexpect "1.9.0"]
-                  [lein-auto "0.1.3"]]
+                  [lein-auto "0.1.3"]
+                  [lein-midje "3.2.1"]]
         :test-refresh {:notify-command ["terminal-notifier" "-title" "Tests" "-message"]
                        :quiet true
                        :changes-only true}}}
