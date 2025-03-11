16788a90afc5e615acbfb2e69340ea6289f90692
Jag G <jag.gunawardana@gmail.com>
2025-03-11 15:46:24 +0000

    Add NGrok

--- provision.sh
+++ provision.sh
@@ -215,3 +215,12 @@ sudo mv kubectl /usr/local/bin
 ####################### fly.io
 curl -L https://fly.io/install.sh | sh
 ln -s /home/vagrant/.fly/bin/fly /home/vagrant/bin/fly
+
+
+####################### NGrok
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
