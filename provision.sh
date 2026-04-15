3dcbd72ec4715a381af192b733bbb7d8489ec9a9
Jag G <jag.gunawardana@gmail.com>
2026-04-15 17:47:54 +0100

    Update

--- provision.sh
+++ provision.sh
@@ -19,6 +19,7 @@ sudo apt-get -y install fortunes figlet
 sudo apt-get -y install redis-server mongodb
 sudo apt-get -y install fonts-powerline
 sudo apt-get -y install awscli nodejs
+sudo apt-get -y install easy-rsa
 sudo snap install --classic emacs
 sudo snap install httpie
 sudo locale-gen en_GB.UTF-8
@@ -237,4 +238,5 @@ curl -s https://packages.stripe.dev/api/security/keypair/stripe-cli-gpg/public |
 echo "deb [signed-by=/usr/share/keyrings/stripe.gpg] https://packages.stripe.dev/stripe-cli-debian-local stable main" | sudo tee -a /etc/apt/sources.list.d/stripe.list
 sudo apt update
 sudo apt install stripe
+
 
